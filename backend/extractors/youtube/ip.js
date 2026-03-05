0dd209ca7821c72b3ec0739928a6781222da1e01
subatomic <bhojk924@gmail.com>
2026-03-05 19:50:06 +0530

    changed args for extraction

--- backend/extractors/youtube/ip.js
+++ backend/extractors/youtube/ip.js
@@ -1,9 +1,15 @@
 const extractor = require('../extractor')
-
-
-
 async function yotubeNetworkFallbackExtractor(url){
-    const args = ['-J','--force-ipv4',url]
+   const args = [
+  '-J',
+  '--force-ipv4',
+  '--js-runtime', 'node',
+  '--extractor-args', 'youtube:player_client=android',
+  '--user-agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)',
+  '--referer', 'https://www.youtube.com/',
+  '--add-header', 'Accept-Language:en-US,en;q=0.9',
+  url
+]
     return extractor(url,args)
 }
 module.exports = yotubeNetworkFallbackExtractor
